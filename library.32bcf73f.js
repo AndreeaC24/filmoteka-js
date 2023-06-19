4f908f75e0fde690cc11838ce7dcac1875a73bb3
luminitza24 <118338076+luminitza24@users.noreply.github.com>
2023-06-19 22:14:30 +0000

    Deploying to gh-pages from @ AndreeaC24/filmoteka-js@0b5a08975b24fd54916d37bf3d44008ac3e00f26 🚀

--- library.32bcf73f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.getElementById("checkbox");"true"===localStorage.getItem("darkMode")&&(document.body.classList.add("dark"),e.checked=!0),e.addEventListener("change",(function(){document.body.classList.toggle("dark");var e=document.body.classList.contains("dark");localStorage.setItem("darkMode",e.toString())}))}();
-//# sourceMappingURL=library.32bcf73f.js.map
