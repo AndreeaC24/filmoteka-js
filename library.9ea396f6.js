863c219af6204be203df09ff847333c89334641e
AndreeaC24 <119935685+AndreeaC24@users.noreply.github.com>
2023-06-19 20:56:32 +0000

    Deploying to gh-pages from @ AndreeaC24/filmoteka-js@d90339d9aed9f6969815ebbf431f33d4e51afb4c 🚀

---
+++ library.9ea396f6.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=document.getElementById("toggleLink"),t=document.getElementById("hiddenDiv");e.addEventListener("click",(function(e){e.preventDefault(),"flex"===t.style.display?t.style.display="none":(t.style.display="flex",t.scrollIntoView({behavior:"smooth",block:"start"}))}))}));
+//# sourceMappingURL=library.9ea396f6.js.map
